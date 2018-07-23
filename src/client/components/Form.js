963147060789bad147d92e78d34b5e7d60f663f7
Sierra Kilo <sierrakilo@Sierras-MBP.DFMNET01>
2018-07-23 09:28:18 -0700

    added axios to manage post and gets, handle form submit in Form.js

--- src/client/components/Form.js
+++ src/client/components/Form.js
@@ -1,21 +1,87 @@
-import React from 'react';
+import React from 'react'
+// import Materialize from 'materialize-css'
+import axios from 'axios'
 import Button from './Button'
 
-const handleSubmit = (e) => {
-  e.preventDefault()
-  alert("you clicked submit")
+// function handleChange(e) {
+//     Materialize.updateTextFields();
+//   }
+
+export default class EmailForm extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      email: '',
+      error: ''
+    }
+    this.onEmailChange = this.onEmailChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+  onEmailChange = (e) => {
+    // Materialize.updateTextFields();
+    const email = e.target.value;
+    this.setState(() => ({ email }))
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const email = this.state.email;
+
+    axios.post("/api/emails/", {
+      email
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
+  render() {
+    return (
+      <div>
+
+        <form onSubmit={(e) => {
+          this.handleSubmit(e)
+          this.props.changeState()
+        }}>
+          <input
+            id="email"
+            type="email"
+            // className="validate"
+            name="email"
+            autoFocus
+            value={this.state.email}
+            onChange={this.onEmailChange}
+          />
+          <Button/>
+          {/* <button type='submit' className="waves-effect waves-light btn button">Add Email</button> */}
+        </form>
+      </div>
+    )
+  }
 }
 
-const Form = (props) => (
-  <div>
-    <form onSubmit={(e) => {
-          handleSubmit(e)
-          props.changeState()
-        }}>
-      <input></input>
-      <Button/>
-    </form>
-  </div>
-);
-
-export default Form;
+// import React from 'react';
+// import Button from './Button'
+//
+// const handleSubmit = (e) => {
+//   e.preventDefault()
+//   alert("you clicked submit")
+// }
+//
+// const Form = (props) => (
+//   <div>
+//     <form onSubmit={(e) => {
+//           handleSubmit(e)
+//           props.changeState()
+//         }}>
+//       <input></input>
+//       <Button/>
+//     </form>
+//   </div>
+// );
+//
+// export default Form;
--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,31 @@
 const express = require('express');
-const os = require('os');
+require('dotenv').config()
+const bodyParser = require('body-parser')
+let PORT = process.env.PORT || 8080;
 
 const app = express();
 
 app.use(express.static('dist'));
-app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
-app.listen(8080, () => console.log('Listening on port 8080!'));
+
+//Models
+const db = require("./models");
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// Sync Database
+db.sequelize.sync().then(function() {
+    console.log('Nice! Database looks fine')
+}).catch(function(err) {
+    console.log(err, "Something went wrong with the Database Update!")
+});
+
+
+
+//routes
+require('./routes/api-routes.js')(app)
+
+app.listen(PORT, () => console.log('Listening on port: ' + PORT));
