b50c0436912f35aabd585c844df22df1b840cd02
Sierra Kilo <sierrakilo@Sierras-MBP.DFMNET01>
2018-07-25 09:54:37 -0700

    admin page lists emails

--- src/client/components/AdminPage.js
+++ src/client/components/AdminPage.js
@@ -2,9 +2,36 @@ import React, { Component } from 'react';
 
 class Admin extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+    allEmails: []
+  }
+}
+
+  componentDidMount() {
+    fetch("/api/emails/")
+      .then(res => res.json())
+      .then(parsedJSON => parsedJSON.map(emails => ({
+        email: `${emails.email}`,
+        id: `${emails.id}`
+      }))).then(emails => this.setState({allEmails: emails}))
+  }
+
+
   render() {
+    console.log(this.state.allEmails);
     return (
-      <div>Admin</div>
+      <div>
+        <h2>Admin</h2>
+        <div>
+          <ul>
+            { this.state.allEmails.map((email) => {
+              return <li key={email.id}>{email.email}</li>
+            })}
+          </ul>
+        </div>
+      </div>
     );
   }
 
